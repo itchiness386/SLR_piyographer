1470a1b87f2d5d23c686640145b31bb3aaffd1db
itchiness386 <itchiness386@gmail.com>
2020-03-10 18:02:14 +0900

    [Add] usersコントローラ作成(user/admin)

---
+++ app/controllers/users/users_controller.rb
@@ -0,0 +1,18 @@
+class Users::UsersController < ApplicationController
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def confirm
+  end
+
+  def delete
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,13 @@ Rails.application.routes.draw do
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
+  namespace :admins do
+  	resources :users, only: [:index, :show, :edit, :update]
+  end
+  scope module: :users do
+    get 'users/confirm' => 'users#confirm'
+    patch 'users/delete' => 'users#delete'
+  	resources :users, only: [:show, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
