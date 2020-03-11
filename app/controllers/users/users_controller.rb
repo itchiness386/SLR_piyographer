35a10437a878936af597a2b2352cdb4eaf745c44
itchiness386 <itchiness386@gmail.com>
2020-03-11 12:09:47 +0900

    [Add] ユーザ情報編集画面追加(user)

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -4,15 +4,26 @@ class Users::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+  	user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user_path(user)
+    else
+      render 'edit'
+    end
   end
 
   def confirm
   end
 
   def delete
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :profile_image, :introduction, :status)
   end
 
 end
