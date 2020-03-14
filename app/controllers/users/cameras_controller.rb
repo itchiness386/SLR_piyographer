73ebb3de80a06203d43af4f6ce31d7ab00eb1b74
itchiness386 <itchiness386@gmail.com>
2020-03-14 16:44:26 +0900

    [Add] カメラ情報編集画面追加(user)

--- app/controllers/users/cameras_controller.rb
+++ app/controllers/users/cameras_controller.rb
@@ -6,23 +6,33 @@ class Users::CamerasController < ApplicationController
   end
 
   def create
-  	@camera = Camera.new(camera_params)
-  	@camera.user_id = current_user.id
-    if @camera.save
+  	camera = Camera.new(camera_params)
+  	camera.user_id = current_user.id
+    if camera.save
       redirect_to cameras_path
     else
-      @cameras = Camera.all
+      cameras = Camera.all
       render 'index'
     end
   end
 
   def edit
+  	@camera = Camera.find(params[:id])
   end
 
   def update
+  	camera = Camera.find(params[:id])
+    if camera.update(camera_params)
+      redirect_to cameras_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    camera = Camera.find(params[:id])
+    camera.destroy
+    redirect_to cameras_path
   end
 
   private
