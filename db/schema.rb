f867534021baea6ad6f1ce77cc69114a9e4ec626
itchiness386 <itchiness386@gmail.com>
2020-03-14 15:25:27 +0900

    [Add] Cameraモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_105946) do
+ActiveRecord::Schema.define(version: 2020_03_14_061604) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2020_03_09_105946) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cameras", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "manufacturer"
+    t.string "modelname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
