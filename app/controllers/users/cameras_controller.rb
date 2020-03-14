ae3c1f9833cedba43be54710658e425171af98e6
itchiness386 <itchiness386@gmail.com>
2020-03-14 16:43:42 +0900

    [Add] カメラ一覧画面追加(user)

--- app/controllers/users/cameras_controller.rb
+++ app/controllers/users/cameras_controller.rb
@@ -1,9 +1,19 @@
 class Users::CamerasController < ApplicationController
 
   def index
+  	@cameras = Camera.all
+  	@camera = Camera.new
   end
 
   def create
+  	@camera = Camera.new(camera_params)
+  	@camera.user_id = current_user.id
+    if @camera.save
+      redirect_to cameras_path
+    else
+      @cameras = Camera.all
+      render 'index'
+    end
   end
 
   def edit
@@ -13,6 +23,12 @@ class Users::CamerasController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def camera_params
+  params.require(:camera).permit(:user_id, :manufacturer, :modelname)
   end
 
 end
