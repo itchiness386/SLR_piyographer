cc5523b6e5898911af2c7bc59c71b279c0a8bb34
itchiness386 <itchiness386@gmail.com>
2020-03-20 14:32:40 +0900

    [Add] ユーザ詳細画面に投稿一覧追加(user/admin)・投稿一覧画面(admin)のレイアウト調整

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,6 +2,7 @@ class Users::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @photos = Photo.where(user_id: @user.id).order(created_at: :desc)
   end
 
   def edit
