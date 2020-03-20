3dd8fdad417a9c934eb2969182d24e3aa46ad7c1
itchiness386 <itchiness386@gmail.com>
2020-03-20 12:18:14 +0900

    [Add] 投稿情報編集画面追加(admin)・投稿情報編集ボタンを投稿詳細画面(admin)に追加

--- app/controllers/admins/photos_controller.rb
+++ app/controllers/admins/photos_controller.rb
@@ -7,9 +7,22 @@ class Admins::PhotosController < ApplicationController
   end
 
   def edit
+  	@photo = Photo.find(params[:id])
   end
 
   def update
+  	photo = Photo.find(params[:id])
+    if photo.update(photo_params)
+      redirect_to admins_photo_path(photo)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:status)
   end
 
 end
