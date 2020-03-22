d2ead93189ef0b68b33fc939cd824d664fe2248b
itchiness386 <itchiness386@gmail.com>
2020-03-22 16:28:24 +0900

    [Add] Favoritesコントローラ作成(user)

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   	resources :cameras, except: [:new, :show]
   	resources :photos, except: [:index] do
       resources :photo_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
