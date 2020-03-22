14598d5991ec82ecfd7ebe2c73d92b3ad4f86383
itchiness386 <itchiness386@gmail.com>
2020-03-22 17:16:35 +0900

    [Add] 投稿詳細画面(user/admin)にいいね機能を追加

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -1,9 +1,18 @@
 class Users::FavoritesController < ApplicationController
 
   def create
+    photo = Photo.find(params[:photo_id])
+    favorite = Favorite.new(photo_id: photo.id)
+    favorite.user_id = current_user.id
+    favorite.save
+    redirect_to photo_path(photo)
   end
 
   def destroy
+    photo = Photo.find(params[:photo_id])
+    favorite = Favorite.find_by(photo_id: photo.id, user_id: current_user.id)
+    favorite.destroy
+    redirect_to photo_path(photo)
   end
 
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,4 +6,8 @@ class Photo < ApplicationRecord
   has_many :photo_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
 end
