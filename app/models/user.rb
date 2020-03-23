d1d40bba70e307b24db86e0eeec13906ccd96246
itchiness386 <itchiness386@gmail.com>
2020-03-23 17:57:39 +0900

    [Add] フォローする/フォロー外す/フォロー確認のメソッド定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,21 @@ class User < ApplicationRecord
   has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
   has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
 
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+
   def remember_me
     true
   end
