304a23c001d80d15e73e7d170329d8ed88527db2
itchiness386 <itchiness386@gmail.com>
2020-03-23 17:55:10 +0900

    [Add] アソシエーション設定(User/Relationship)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   has_many :photos, dependent: :destroy
   has_many :photo_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
 
   def remember_me
     true
