b4d15c843c657aa0dfbd872ddd07ae92ed68e6e9
itchiness386 <itchiness386@gmail.com>
2020-03-27 17:04:24 +0900

    [Add] Usersテーブルにimpressions_countカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_150536) do
+ActiveRecord::Schema.define(version: 2020_03_26_152007) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2020_03_26_150536) do
     t.string "profile_image_id"
     t.text "introduction"
     t.boolean "status", default: true
+    t.integer "impressions_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
