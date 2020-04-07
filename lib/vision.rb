18d40653fbd8146c186ebc9624c4ca8d11c646aa
itchiness386 <itchiness386@gmail.com>
2020-04-07 23:44:39 +0900

    [Fix] 画像URLの呼び出し表記再々々々々修正

--- lib/vision.rb
+++ lib/vision.rb
@@ -6,11 +6,7 @@ module Vision
     def get_image_data(image_file)
       api_url = "https://vision.googleapis.com/v1/images:annotate?key=#{ENV['GOOGLE_VISION_API_KEY']}"
       # 画像をbase64にエンコード
-      if Rails.env.production?
-        base64_image = Base64.encode64(open("http://18.176.43.221/public/uploads/#{image_file.id}").read)
-      else
-        base64_image = Base64.encode64(open("#{Rails.root}/public/uploads/#{image_file.id}").read)
-      end
+      base64_image = Base64.encode64(open("#{Rails.root}/public/uploads/#{image_file.id}").read)
       # APIリクエスト用のJSONパラメータ
       params = {
         requests: [{
