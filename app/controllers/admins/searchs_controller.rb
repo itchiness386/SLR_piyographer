85215d659d5dd56069e91529503b56a8e05434d5
itchiness386 <itchiness386@gmail.com>
2020-04-07 16:17:03 +0900

    [Add] カメラ/タグ検索機能追加

--- app/controllers/admins/searchs_controller.rb
+++ app/controllers/admins/searchs_controller.rb
@@ -3,7 +3,9 @@ class Admins::SearchsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @keyword = params[:q][:name_cont]
+    @user_keyword = params[:q][:name_cont]
+    @camera_keyword = params[:q][:camera_cont]
+    @tag_keyword = params[:q][:tags_name_cont]
   end
 
 end
--- app/controllers/users/searchs_controller.rb
+++ app/controllers/users/searchs_controller.rb
@@ -3,7 +3,9 @@ class Users::SearchsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @keyword = params[:q][:name_cont]
+    @user_keyword = params[:q][:name_cont]
+    @camera_keyword = params[:q][:camera_cont]
+    @tag_keyword = params[:q][:tags_name_cont]
   end
 
 end
